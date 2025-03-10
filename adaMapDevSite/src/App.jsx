4e2154f8582321f6b0eb0b425a6b9966fc2cebb0
windee <ezhu0922@gmail.com>
2025-03-09 20:57:34 -0400

    forgot to add back importing "WalkingRouteMap"

--- adaMapDevSite/src/App.jsx
+++ adaMapDevSite/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
+import WalkingRouteMap from "./WalkingRouteMap";
 
 function App() {
   const [highContrast] = useState(false);
